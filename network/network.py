df33fe09d49f3be6ea3ab9ce1e65b6582eaf4d21
DaniilMP3 <daniilpurisov390@gmail.com>
2023-05-01 22:54:02 +0300

    start making backprop

--- network/network.py
+++ network/network.py
@@ -8,6 +8,7 @@ class Network:
         self.layer_sizes = layer_sizes
         self.biases = [np.random.randn(y, 1) for y in layer_sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
+        print('lalala')
 
     def SGD(self, training_data, epochs, batch_size, eta, test_data=None):
         n = len(training_data)
@@ -18,7 +19,41 @@ class Network:
                 self.update_mini_batch(mini_batch, eta)
 
     def update_mini_batch(self, mini_batch, eta):
-        pass
+        test_data = np.random.random((1, 2))
+        outputs = np.random.random((1, 2))
+
+        self.backprop(test_data, outputs)
+
+
+    @staticmethod
+    def sigmoid(z):
+        return 1.0/(1.0+np.exp(-z))
+
+    def sigmoid_derivative(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    def backprop(self, x, y):
+        """
+        x - training inputs
+        y - outputs from training data
+        """
+        activation = x
+        activations = [x]
+        zs_vectors = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs_vectors.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+
+        for l in range(2, self.num_layers):
+            z = zs_vectors[-l]
+            sp = self.sigmoid_derivative(z)
+
+
+
+
+
 
 
 
